76512e134426573a08992dd9818926ec8ef9b17d
theBitCoiner <thebitcoinerapps@gmail.com>
2019-04-13 17:35:35 +0200

    handling props

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -18,21 +18,28 @@ class Forecast extends React.Component{
             <div className="forecast-container">
             <Card className="forecast-card">
             <h4 className="text-day">{this.convertTime(this.forecastData.data.daily.data[0].time)}</h4>
+            <h3 className="text-summary">{this.forecastData.data.daily.data[0].summary}</h3>
+            <h4 className="text-day">{`${Math.round((this.forecastData.data.daily.data[0].temperatureHigh-32)/1.8)} C`}</h4>
                 
             </Card>
 
             <Card className="forecast-card">
             <h4 className="text-day">{this.convertTime(this.forecastData.data.daily.data[1].time)}</h4>
-                
+            <h3 className="text-summary">{this.forecastData.data.daily.data[1].summary}</h3>
+            <h4 className="text-day">{`${Math.round((this.forecastData.data.daily.data[1].temperatureHigh-32)/1.8)} C`}</h4>
             </Card>
 
             <Card className="forecast-card">
             <h4 className="text-day">{this.convertTime(this.forecastData.data.daily.data[2].time)}</h4>
+            <h3 className="text-summary">{this.forecastData.data.daily.data[2].summary}</h3>
+            <h4 className="text-day">{`${Math.round((this.forecastData.data.daily.data[2].temperatureHigh-32)/1.8)} C`}</h4>
                 
             </Card>
 
             <Card className="forecast-card">
             <h4 className="text-day">{this.convertTime(this.forecastData.data.daily.data[3].time)}</h4>
+            <h3 className="text-summary">{this.forecastData.data.daily.data[3].summary}</h3>
+            <h4 className="text-day">{`${Math.round((this.forecastData.data.daily.data[3].temperatureHigh-32)/1.8)} C`}</h4>
                 
             </Card>
             
